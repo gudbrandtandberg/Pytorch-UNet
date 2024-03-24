6d6c4def0c7fccf30c5508fd70b722ee955f5616
Gudbrand Tandberg <gudbrand@3lc.ai>
2024-03-24 18:02:22 +0100

    Revert some changes

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn.functional as F
 from tqdm import tqdm
-from utils.dice_score import dice_coeff, multiclass_dice_coeff
+
+from utils.dice_score import multiclass_dice_coeff, dice_coeff
 
 
 @torch.inference_mode()
@@ -11,9 +12,9 @@ def evaluate(net, dataloader, device, amp):
     dice_score = 0
 
     # iterate over the validation set
-    with torch.autocast(device.type if device.type != "mps" else "cpu", enabled=amp):
-        for batch in tqdm(dataloader, total=num_val_batches, desc="Validation round", unit="batch", leave=False):
-            image, mask_true = batch["image"], batch["mask"]
+    with torch.autocast(device.type if device.type != 'mps' else 'cpu', enabled=amp):
+        for batch in tqdm(dataloader, total=num_val_batches, desc='Validation round', unit='batch', leave=False):
+            image, mask_true = batch['image'], batch['mask']
 
             # move images and labels to correct device and type
             image = image.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
@@ -23,14 +24,12 @@ def evaluate(net, dataloader, device, amp):
             mask_pred = net(image)
 
             if net.n_classes == 1:
-                assert mask_true.min() >= 0 and mask_true.max() <= 1, "True mask indices should be in [0, 1]"
-                mask_pred = (torch.sigmoid(mask_pred) > 0.5).float()
+                assert mask_true.min() >= 0 and mask_true.max() <= 1, 'True mask indices should be in [0, 1]'
+                mask_pred = (F.sigmoid(mask_pred) > 0.5).float()
                 # compute the Dice score
                 dice_score += dice_coeff(mask_pred, mask_true, reduce_batch_first=False)
             else:
-                assert (
-                    mask_true.min() >= 0 and mask_true.max() < net.n_classes
-                ), "True mask indices should be in [0, n_classes["
+                assert mask_true.min() >= 0 and mask_true.max() < net.n_classes, 'True mask indices should be in [0, n_classes['
                 # convert to one-hot format
                 mask_true = F.one_hot(mask_true, net.n_classes).permute(0, 3, 1, 2).float()
                 mask_pred = F.one_hot(mask_pred.argmax(dim=1), net.n_classes).permute(0, 3, 1, 2).float()
